780493c39f264eb60ac1ea520534aafe50b7fd77
brennopacheco <brenno_pacheco@hotmail.com>
2022-06-13 12:02:58 -0300

    Initial commit

---
+++ src/cursoemvideo/POOjava/Aula02/Objeto/CanetaTest.java
@@ -0,0 +1,25 @@
+package cursoemvideo.POOjava.Aula02.Objeto;
+
+import cursoemvideo.POOjava.Aula02.Classe.Caneta;
+
+public class CanetaTest {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta();
+        c1.cor = "Azul";
+        c1.ponta = 0.5F;
+        c1.tampar();
+
+        c1.status();
+        c1.rabiscar();
+
+        System.out.println("---------");
+
+        Caneta c2 = new Caneta();
+        c2.modelo = "Hostnet";
+        c2.cor = "Preta";
+        c2.destampar();
+
+        c2.status();
+        c2.rabiscar();
+    }
+}
---
+++ src/cursoemvideo/POOjava/Aula03/Classe/Caneta.java
@@ -0,0 +1,38 @@
+package cursoemvideo.POOjava.Aula03.Classe;
+
+public class Caneta {
+    // criar Classe caneta
+
+    public String modelo;
+    public String cor;
+    public float ponta;
+    public int carga;
+    public boolean tampada;
+
+    public void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+
+    public void rabiscar() {
+        if (tampada == true) {
+            System.out.println("ERRO! Não posso rabiscar");
+        } else {
+            System.out.println("Estou Rabiscando");
+        }
+    }
+
+    public void tampar() {
+        this.tampada = true;
+        /* 'this' é o nome do objeto que chamou. No caso quem está
+        chamando tampada é c1. Então 'this' vai ser substituido por c1.tampada
+        */
+    }
+
+    public void destampar() {
+        this.tampada = false;
+    }
+}
