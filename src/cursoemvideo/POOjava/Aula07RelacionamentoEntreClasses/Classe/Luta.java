1e9aa2736fd31f2951e6d50d5829514b5e08974f
brennopacheco <brenno_pacheco@hotmail.com>
2022-06-15 15:21:35 -0300

    Remove class luta

--- src/cursoemvideo/POOjava/Aula07RelacionamentoEntreClasses/Classe/Luta.java
+++
@@ -1,8 +0,0 @@
-package cursoemvideo.POOjava.Aula07RelacionamentoEntreClasses.Classe;
-
-public class Luta {
-    private String desafiado;
-    private String desafiante;
-    private String rounds;
-    private String aprovada;
-}
