83de56590f19d6bfaae834bfc67e9ccf92df68f8
brennopacheco <brenno_pacheco@hotmail.com>
2022-06-15 15:22:25 -0300

    changing L to L

--- src/cursoemvideo/POOjava/Aula07RelacionamentoEntreClasses/Objeto/LutadorTest.java
+++ src/cursoemvideo/POOjava/Aula07RelacionamentoEntreClasses/Objeto/LutadorTest.java
@@ -5,32 +5,32 @@ import cursoemvideo.POOjava.Aula07RelacionamentoEntreClasses.Classe.Lutador;
 public class LutadorTest {
     public static void main(String[] args) {
 
-        Lutador L[] = new Lutador[6];
+        Lutador l[] = new Lutador[6];
 
-        L[0] = new Lutador("Pretty boy", "França", 31, 1.75, 68.9, 11, 3, 1);
+        l[0] = new Lutador("Pretty boy", "França", 31, 1.75, 68.9, 11, 3, 1);
 
-        L[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2,3);
+        l[1] = new Lutador("Putscript", "Brasil", 29, 1.68, 57.8, 14, 2,3);
 
-        L[2] = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, 12, 2,1);
+        l[2] = new Lutador("Snapshadow", "EUA", 35, 1.65, 80.9, 12, 2,1);
 
-        L[3] = new Lutador("Dead Code", "Australia", 28, 1.93, 81.6, 13, 0,2);
+        l[3] = new Lutador("Dead Code", "Australia", 28, 1.93, 81.6, 13, 0,2);
 
-        L[4] = new Lutador("UF0Cobol", "Brasil", 37, 1.7, 119.3, 5, 4,3);
+        l[4] = new Lutador("UF0Cobol", "Brasil", 37, 1.7, 119.3, 5, 4,3);
 
-        L[5] = new Lutador("Nerdaar", "EUA", 30, 1.81, 105.7, 12, 2,4);
+        l[5] = new Lutador("Nerdaar", "EUA", 30, 1.81, 105.7, 12, 2,4);
 
-        L[3].ganharLuta();
-        L[5].perderLuta();
-        L[0].ganharLuta();
-        L[1].empatarLuta();
+        l[3].ganharLuta();
+        l[5].perderLuta();
+        l[0].ganharLuta();
+        l[1].empatarLuta();
 
 
 
-        L[0].status();
-        L[1].status();
-        L[2].status();
-        L[3].status();
-        L[4].status();
-        L[5].status();
+        l[0].status();
+        l[1].status();
+        l[2].status();
+        l[3].status();
+        l[4].status();
+        l[5].status();
     }
 }
