ad7205cb4023057b2028ff66dc0fa110346ca112
brennopacheco <brenno_pacheco@hotmail.com>
2022-06-17 12:26:09 -0300

    edit method

--- src/cursoemvideo/POOjava/Aula12Polimorfismo/Classe/Mamifero/Cachorro.java
+++ src/cursoemvideo/POOjava/Aula12Polimorfismo/Classe/Mamifero/Cachorro.java
@@ -8,4 +8,9 @@ public class Cachorro extends Mamifero {
     public void abanarRabo(){
         System.out.println("Abanando o rabo");
     }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("Au au au au");
+    }
 }
